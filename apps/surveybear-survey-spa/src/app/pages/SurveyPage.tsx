63d46ea493c65a4b014563244c9a655ead2797e0
E.N <67209588+ElisNz@users.noreply.github.com>
2023-06-14 13:47:05 +0200

    rollback

--- apps/surveybear-survey-spa/src/app/pages/SurveyPage.tsx
+++ apps/surveybear-survey-spa/src/app/pages/SurveyPage.tsx
@@ -1,7 +1,24 @@
 import { useParams } from 'react-router-dom';
+<<<<<<< HEAD
 
 const SurveyPage = () => {
     const { surveyId } = useParams();
+=======
+import { getSurveyById } from '../../api/getSurveyById';
+
+const SurveyPage = () => {
+    const { surveyId } = useParams();
+    const [ survey, setSurvey ] = useState<ISurvey>();
+
+    useEffect(() => {
+        if(surveyId){
+            getSurveyById(surveyId).then(survey => setSurvey(survey));
+        } else {
+            // Visa att det inte finns nåt id
+        }
+    }, [])
+
+>>>>>>> parent of 6be9a6c (small fixes)
     return (<>
         <h1>Survey</h1>
         <p>{surveyId}</p>
